f9352a7ab42f940b363f19bdb1ed4b4364f8c2f9
gwonllna <magunho9677@gmail.com>
2023-02-19 09:55:27 +0900

    perfectplanner 환경설정

---
+++ src/main/java/perfectplanner/perfectplanner/PerfectplannerApplication.java
@@ -0,0 +1,13 @@
+package perfectplanner.perfectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perfectplanner/perfectplanner/PerfectplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package perfectplanner.perfectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
